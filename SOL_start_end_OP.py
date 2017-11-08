e1cdb956e7d046ce6c26825e93f2217e578af75f
karde <kardelen.karatas@gmail.com>
2017-11-08 20:57:09 +0100

    use geometry collection

--- SOL_start_end_OP.py
+++ SOL_start_end_OP.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 import xml.etree.ElementTree as ET
 import json
 import geojson
-from geojson import LineString, FeatureCollection
+from geojson import LineString, GeometryCollection
 tree = ET.parse('RINFsol_lux_mal_nor.xml')
 
 section_of_lines = tree.findall('.//SectionOfLine')
@@ -32,5 +32,5 @@ for section in section_of_lines:
     sol.end['coords'] = (float(end_op_point_coords.get('Longitude').replace(',', '.')), float(end_op_point_coords.get('Latitude').replace(',', '.')))
     line_strings.append(LineString([sol.start['coords'], sol.end['coords']]))
 
-    feature_collection = FeatureCollection(line_strings)
-    print(geojson.dumps(feature_collection))
+geometry_collection = GeometryCollection(line_strings)
+print(geojson.dumps(geometry_collection))
